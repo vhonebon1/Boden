3bbcd388b8df4483054aea4d817bb4870a05507c
verity <verity.honebon@royalacademy.org.uk>
2019-03-12 13:35:33 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  
+  scope '/api' do
+    resources :drinks
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
