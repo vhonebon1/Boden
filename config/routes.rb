419fa3961261bdc409b970bfd7b1e094d0145e62
verity <verity.honebon@royalacademy.org.uk>
2019-03-15 13:53:14 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  
+
   scope '/api' do
     resources :drinks
+  end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
