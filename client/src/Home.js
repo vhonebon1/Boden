85f80dae54a197432119bb7f4462773e18385f23
verity <verity.honebon@royalacademy.org.uk>
2019-03-16 13:17:28 +0000

    finish set up

--- client/src/Home.js
+++ client/src/Home.js
@@ -37,47 +37,12 @@ class Home extends Component {
   }
 
   render () {
-    let {drinks, drink} = this.state
-    return drinks
-      ? <Container text>
-        <div className='howdy'>Yo</div>
-        <Header as='h2' icon textAlign='center' color='teal'>
-          <Icon name='unordered list' circular />
-          <Header.Content>
-            List of Ingredients
-          </Header.Content>
-        </Header>
-        <Divider hidden section />
-        {drinks && drinks.length
-          ? <Button.Group color='teal' fluid widths={drinks.length}>
-            {Object.keys(drinks).map((key) => {
-              return <Button active={drink && drink.id === drinks[key].id} fluid key={key} onClick={() => this.getDrink(drinks[key].id)}>
-                {drinks[key].title}
-              </Button>
-            })}
-          </Button.Group>
-          : <Container textAlign='center'>No drinks found.</Container>
-        }
-        <Divider section />
-        {drink &&
-          <Container>
-            <Header as='h2'>{drink.title}</Header>
-            {drink.description && <p>{drink.description}</p>}
-            {drink.ingredients &&
-              <Segment.Group>
-                {drink.ingredients.map((ingredient, i) => <Segment key={i}>{ingredient.description}</Segment>)}
-              </Segment.Group>
-            }
-            {drink.steps && <p>{drink.steps}</p>}
-            {drink.source && <Button basic size='tiny' color='teal' href={drink.source}>Source</Button>}
-          </Container>
-        }
-      </Container>
-      : <Container text>
-        <Dimmer active inverted>
-          <Loader content='Loading' />
-        </Dimmer>
-      </Container>
+    const { drinks } = this.state
+    return(
+      <div>
+        { drinks && drinks.map((drink) => <div>{drink.title}</div>)}
+    </div>
+    )
   }
 }
 
